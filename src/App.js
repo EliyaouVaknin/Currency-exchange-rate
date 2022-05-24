32053d0437736cbcdea1739c672e243d8d1c3bb2
Eliyaou Vaknin <eliyaou5423703@gmail.com>
2022-05-25 00:12:09 +0300

    Added frontend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import './App.css';
-import Header from './Components/Header'
-import DateInput from './Components/DateInput'
-import Chart from './Components/Chart';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import DateInput from "./Components/DateInput";
+import Chart from "./Components/Chart";
 
 function App() {
+  const [weekDataEUR_USD, setWeekDataEUR_USD] = useState([]);
+  const [weekDataUSD_GBP, setWeekDataUSD_GBP] = useState([]);
+  const [weekDaysName, setWeekDaysName] = useState([]);
+  const [fromDate, setFromDate] = useState("");
+  const [toDate, setToDate] = useState("");
+  const apiKey = "e6d56476b9a191585468";
+
+  const convertCurrency = async (fromCurrency, toCurrency) => {
+    fromCurrency = encodeURIComponent(fromCurrency);
+    toCurrency = encodeURIComponent(toCurrency);
+    var query = fromCurrency + "_" + toCurrency;
+
+    var url =
+      "https://free.currconv.com/api/v7/convert?q=" +
+      query +
+      "&compact=ultra&date=" +
+      fromDate +
+      "&endDate=" +
+      toDate +
+      "&apiKey=" +
+      apiKey;
+
+    await fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.EUR_USD != null) {
+          setWeekDataEUR_USD(Object.values(res.EUR_USD));
+          setWeekDaysName(Object.keys(res.EUR_USD));
+        } else {
+          setWeekDataUSD_GBP(Object.values(res.USD_GBP));
+        }
+      });
+  };
+
   return (
     <div className="App">
       <Header />
-      <DateInput />
-      <Chart />
+      <DateInput
+        setFromDate={setFromDate}
+        setToDate={setToDate}
+        convertCurrency={convertCurrency}
+      />
+      <Chart
+        weekDataEUR_USD={weekDataEUR_USD}
+        weekDataUSD_GBP={weekDataUSD_GBP}
+        weekDaysName={weekDaysName}
+      />
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,13 +4,13 @@ import "../Styles/Header.css"
 export default function Header() {
   
   return (
-    <div className="container">
+    <div className="container-fluid">
       <div className="row">
-        <div className="col-md-2">
-          <img src="../Images/logo.webp" />
+        <div className="col-md-3">
+        <img className="img-fluid" src={require('../Images/logo.webp')}></img>
         </div>
-        <div className="col-md-10">
-          <h3>Chainwire exchange rate</h3>
+        <div className="col-md-9" style={{textAlign: "left"}}>
+          <h2>Exchange rate application</h2>
         </div>
       </div>
     </div>
