2c403600f5872acd10d5a527651607e6361fb5c1
Eliyaou Vaknin <eliyaou5423703@gmail.com>
2022-05-26 12:35:47 +0300

    added backend

---
+++ Client/src/Components/Chart.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import "../Styles/Chart.css";
+
+export default function Chart({
+  weekDataEUR_USD,
+  weekDataUSD_GBP,
+  weekDaysName,
+}) {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  return (
+    <div className="ChartContainer container card">
+      <div className="row">
+        <div className="col-md-6">
+          <h3 style={{textAlign: 'center'}}>EUR_USD</h3>
+          <Line
+            data={{
+              labels: [
+                weekDaysName[0],
+                weekDaysName[1],
+                weekDaysName[2],
+                weekDaysName[3],
+                weekDaysName[4],
+                weekDaysName[5],
+                weekDaysName[6],
+              ],
+              datasets: [
+                {
+                  label: "Exchange Rate",
+                  data: weekDataEUR_USD,
+                  fill: true,
+                  backgroundColor: "rgba(75,192,192,0.2)",
+                  borderColor: "rgba(75,192,192,1)",
+                },
+              ],
+            }}
+          />
+        </div>
+        <div className="col-md-6">
+          <h3 style={{textAlign: 'center'}}>USD_GBP</h3>
+          <Line
+            data={{
+              labels: [
+                weekDaysName[0],
+                weekDaysName[1],
+                weekDaysName[2],
+                weekDaysName[3],
+                weekDaysName[4],
+                weekDaysName[5],
+                weekDaysName[6],
+              ],
+              datasets: [
+                {
+                  label: "Exchange Rate",
+                  data: weekDataUSD_GBP,
+                  fill: true,
+                  backgroundColor: "rgba(75,192,192,0.2)",
+                  borderColor: "rgba(75,192,192,1)",
+                },
+              ],
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ Client/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Styles/Header.css";
+
+export default function Header() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-3">
+          <img className="img-fluid" src={require("../Images/logo.webp")}></img>
+        </div>
+        <div className="col-md-9" style={{ textAlign: "left" }}>
+          <h2>Exchange rate application</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
